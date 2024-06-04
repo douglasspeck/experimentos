1c609b16cbe4be7c7805c0259074a6e06e6b2c09
Douglas Speck <speck.empresarial@gmail.com>
2024-06-04 15:53:16 -0300

    First Commit

---
+++ forms/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>Formulários de Coleta</title>
+        
+        <!-- META TAGS -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="theme-color" content="rgb(82, 182, 15)">
+
+        <!-- SEO -->
+        <meta name="author" content="Douglas Speck">
+        <meta name="description" content="Índice de coletas do exp.online">
+        <link rel="canonical" href="https://forms.experimentos.online">
+        <meta name=”robots” content="index, nofollow">
+        <meta name="googlebot" content="index, nofollow">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Martel:wght@200;300;400;600;700;800;900&family=Not+Sans:ital,wght@0,100..900;1,100..900&family=Shadows+Into+Light+Two&display=swap" rel="stylesheet">
+
+        <!-- Stylesheets -->
+
+        <link rel="preload" as="style" onload="this.remove();" href="/assets/css/main.css?t=<?php echo date('YmdHis'); ?>" type="text/css">
+        <link rel="stylesheet" href="/assets/css/main.css?t=<?php echo date('YmdHis'); ?>" type="text/css">
+    </head>
+    <body>
+        <h1>Formulários de Coleta</h1>
+        <p>Sua curiosidade te trouxe até aqui, mas não há muito para ver. Acesse um de nossos experimentos abaixo:</p>
+        <ul>
+            <li><a href="1.html">Experimento 1: Projeto Interativo (PI)</a></li>
+        </ul>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>exp.online</title>
+        
+        <!-- META TAGS -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="theme-color" content="rgb(82, 182, 15)">
+
+        <!-- SEO -->
+        <meta name="author" content="Douglas Speck">
+        <meta name="description" content="Experimentos Online construídos de forma coletiva para divulgar conhecimentos matemáticos">
+        <link rel="canonical" href="https://experimentos.online">
+        <meta name=”robots” content="index, nofollow">
+        <meta name="googlebot" content="index, nofollow">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Martel:wght@200;300;400;600;700;800;900&family=Not+Sans:ital,wght@0,100..900;1,100..900&family=Shadows+Into+Light+Two&display=swap" rel="stylesheet">
+
+        <!-- Stylesheets -->
+
+        <link rel="preload" as="style" onload="this.remove();" href="/assets/css/main.css?t=<?php echo date('YmdHis'); ?>" type="text/css">
+        <link rel="stylesheet" href="/assets/css/main.css?t=<?php echo date('YmdHis'); ?>" type="text/css">
+    </head>
+    <body>
+        <main>
+            <p>baseado em nosso <a href="/forms/1.html">primeiro experimento</a>:</p>
+            <p>&pi;=<span class="correct">3.14159</span><span class="incorrect">00000</span></p>
+            <p>(atualmente, há 10 respostas)</p>
+        </main>
+        <footer>
+            <p>este projeto foi orgulhosamente produzido por <a href="https://tresdoug.com">Douglas Speck</a></p>
+        </footer>
+    </body>
+</html>
